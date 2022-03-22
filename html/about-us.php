fb1da5b1f1fbe33bd843a080f52c8dd9f856a824
JOM3C4 <jordimejia655@gmail.com>
2022-03-22 12:57:32 +0100

    update

--- html/about-us.php
+++ html/about-us.php
@@ -1,9 +1,24 @@
-<div>
-    <h1>Laura</h1>
-</div>
-<div>
-    <h1>Jordy</h1>
-</div>
-<div>
-    <h1>Marti</h1>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos/about-us.css">
+    <title>About Us</title>
+</head>
+<body>
+    <div class="contenedor">
+        <header>
+            <div class="card">
+                <h2>CONÓCENOS</h2>
+                <p>Conoce al equipo que trabajará contigo día a día 
+                    para hacer que tus objetivos sean una realidad
+                </p>
+                <img src="img/fitness.jpg" alt="culturismo">
+            </div>
+        </header>
+
+    </div>
+</body>
+</html>

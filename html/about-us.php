eca42317e76d33150349bb4e6d3687b2e3ce8c77
strom99 <alissoncc17@gmail.com>
2022-03-22 23:45:39 +0100

    camvios

--- html/about-us.php
+++ html/about-us.php
@@ -1,5 +1,19 @@
 <div>
+    <img src="" alt="">
     <h1>Laura</h1>
+    <span>Full Stack Developer</span>
+    <div>
+        <a href="#">
+            <i style="color:black" class="fa-brands fa-instagram"></i>
+        </a>
+        <a href="#">
+            <i style="color:black" class="fa-solid fa-envelope"></i>
+        </a>
+        <a href="">
+            <i style="color:black" class="fa-brands fa-linkedin"></i>
+        </a>
+    </div>
+    <p>hello, my name is laura,in charge of the functionality of the page and the database. Excited about keeping this project going</p>
 </div>
 <div>
     <h1>Jordy</h1>

ec32c204a1438867236a9032b403933b266f394e
strom99 <alissoncc17@gmail.com>
2022-03-23 00:05:07 +0100

    DDD

--- html/about-us.php
+++ html/about-us.php
@@ -1,27 +1,3 @@
-<div>
-    <img src="" alt="">
-    <h1>Laura</h1>
-    <span>Full Stack Developer</span>
-    <div>
-        <a href="#">
-            <i style="color:black" class="fa-brands fa-instagram"></i>
-        </a>
-        <a href="#">
-            <i style="color:black" class="fa-solid fa-envelope"></i>
-        </a>
-        <a href="">
-            <i style="color:black" class="fa-brands fa-linkedin"></i>
-        </a>
-    </div>
-    <p>hello, my name is laura,in charge of the functionality of the page and the database. Excited about keeping this project going</p>
-</div>
-<div>
-    <h1>Jordy</h1>
-</div>
-<div>
-    <h1>Marti</h1>
-</div>
-
 <div class="contenedor">
     <header>
         <div class="card">
@@ -32,4 +8,21 @@
             <img src="img/fitness.jpg" alt="culturismo">
         </div>
     </header>
+    <div>
+        <img src="" alt="">
+        <h1>Laura</h1>
+        <span>Full Stack Developer</span>
+        <div>
+            <a href="#">
+                <i style="color:black" class="fa-brands fa-instagram"></i>
+            </a>
+            <a href="#">
+                <i style="color:black" class="fa-solid fa-envelope"></i>
+            </a>
+            <a href="">
+                <i style="color:black" class="fa-brands fa-linkedin"></i>
+            </a>
+        </div>
+        <p>hello, my name is laura,in charge of the functionality of the page and the database. Excited about keeping this project going</p>
+    </div>
 </div>

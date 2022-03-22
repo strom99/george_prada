490959f347cb273b400e7eb39bf2fbea2d2d2727
JOM3C4 <jordimejia655@gmail.com>
2022-03-23 00:53:11 +0100

    update

--- html/about-us.php
+++ html/about-us.php
@@ -1,28 +1,75 @@
 <div class="contenedor">
     <header>
         <div class="card">
-            <h2>CONÓCENOS</h2>
-            <p>Conoce al equipo que trabajará contigo día a día
-                para hacer que tus objetivos sean una realidad
-            </p>
             <img src="img/fitness.jpg" alt="culturismo">
+            <div class="titulo">
+                <h1>CONÓCENOS</h1>
+                <p>Conoce al equipo que trabajará contigo día a día
+                    para hacer que tus objetivos sean una realidad
+                </p>
+            </div>
         </div>
     </header>
-    <div>
-        <img src="" alt="">
-        <h1>Laura</h1>
-        <span>Full Stack Developer</span>
-        <div>
-            <a href="#">
-                <i style="color:black" class="fa-brands fa-instagram"></i>
-            </a>
-            <a href="#">
-                <i style="color:black" class="fa-solid fa-envelope"></i>
-            </a>
-            <a href="">
-                <i style="color:black" class="fa-brands fa-linkedin"></i>
-            </a>
+    <main>
+        <div class="card-1">
+            <div class="titulo-2">
+                <!-- <img src="" alt=""> -->
+                <h2>Laura</h2>
+                <span>Full Stack Developer</span>
+            </div>
+            <div>
+                <a href="#">
+                    <i style="color:black" class="fa-brands fa-instagram"></i>
+                </a>
+                <a href="#">
+                    <i style="color:black" class="fa-solid fa-envelope"></i>
+                </a>
+                <a href="#">
+                    <i style="color:black" class="fa-brands fa-linkedin"></i>
+                </a>
+            </div>
+            <p>hello, my name is laura,in charge of the functionality of the page and the database. Excited about
+                keeping this project going</p>
         </div>
-        <p>hello, my name is laura,in charge of the functionality of the page and the database. Excited about keeping this project going</p>
-    </div>
+        <div class="card-1">
+            <div class="titulo-2">
+                <!-- <img src="" alt=""> -->
+                <h2>Marti</h2>
+                <span>Full Stack Developer</span>
+            </div>
+            <div>
+                <a href="#">
+                    <i style="color:black" class="fa-brands fa-instagram"></i>
+                </a>
+                <a href="#">
+                    <i style="color:black" class="fa-solid fa-envelope"></i>
+                </a>
+                <a href="#">
+                    <i style="color:black" class="fa-brands fa-linkedin"></i>
+                </a>
+            </div>
+            <p>hello, my name is laura,in charge of the functionality of the page and the database. Excited about
+                keeping this project going</p>
+        </div>
+        <div class="card-1">
+            <div class="titulo-2">
+                <!-- <img src="" alt=""> -->
+                <h2>Jordy</h2>
+                <span>Full Stack Developer</span>
+            </div>
+            <div>
+                <a href="#">
+                    <i style="color:black" class="fa-brands fa-instagram"></i>
+                </a>
+                <a href="#">
+                    <i style="color:black" class="fa-solid fa-envelope"></i>
+                </a>
+                <a href="#">
+                    <i style="color:black" class="fa-brands fa-linkedin"></i>
+                </a>
+            </div>
+            <p>hello, my name is laura,in charge of the functionality of the page and the database. Excited about
+                keeping this project going</p>
+        </div>
+    </main>
 </div>

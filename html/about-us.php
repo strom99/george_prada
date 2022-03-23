9bcb64fdd1d02d24d6528365fc64179754e9a3b5
JOM3C4 <jordimejia655@gmail.com>
2022-03-23 09:25:04 +0100

    update

--- html/about-us.php
+++ html/about-us.php
@@ -8,8 +8,9 @@
             <img src="img/fitness.jpg" alt="culturismo">
         </div>
     </header>
-    <div>
-        <img style="width: 200px;" src="img/alison.jpg" alt="">
+    <main>
+    <div class="card">
+        <img style="width: 200px;" src="img/alison.jpg" alt="imagen-1">
         <h1>Laura</h1>
         <span>Full Stack Developer</span>
         <div>
@@ -25,4 +26,43 @@
         </div>
         <p>hello, my name is laura,in charge of the functionality of the page and the database. Excited about keeping this project going</p>
     </div>
+    <div class="card">
+        <div class="title">
+        <img style="width: 200px;" src="img/alison.jpg" alt="imagen-1">
+        <h1>Jordi</h1>
+        <span>Full Stack Developer</span>
+        </div>
+        <div>
+            <a href="#">
+                <i style="color:black" class="fa-brands fa-instagram"></i>
+            </a>
+            <a href="#">
+                <i style="color:black" class="fa-solid fa-envelope"></i>
+            </a>
+            <a href="">
+                <i style="color:black" class="fa-brands fa-linkedin"></i>
+            </a>
+        </div>
+        <p>hello, my name is laura,in charge of the functionality of the page and the database. Excited about keeping this project going</p>
+    </div>
+    <div class="card">
+        <div class="title">
+        <img style="width: 200px;" src="img/alison.jpg" alt="imagen-1">
+        <h1>Martí</h1>
+        <span>Full Stack Developer</span>
+        </div>
+        <div>
+            <a href="#">
+                <i style="color:black" class="fa-brands fa-instagram"></i>
+            </a>
+            <a href="#">
+                <i style="color:black" class="fa-solid fa-envelope"></i>
+            </a>
+            <a href="">
+                <i style="color:black" class="fa-brands fa-linkedin"></i>
+            </a>
+        </div>
+        <p>hello, my name is laura,in charge of the functionality of the page and the database. Excited about keeping this project going</p>
+    </div>
+    </main>
 </div>

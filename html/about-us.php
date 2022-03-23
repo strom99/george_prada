b6f4350729826f7dbc9faf2ef7a55fde5a5604fc
MBP2001 <mbaulenasperez@gmail.com>
2022-03-23 12:42:04 +0100

    hola

--- html/about-us.php
+++ html/about-us.php
@@ -25,4 +25,22 @@
         </div>
         <p>hello, my name is laura,in charge of the functionality of the page and the database. Excited about keeping this project going</p>
     </div>
+    <div>
+        <img style="width: 200px;" src="img/Marti.jpg" alt="">
+        <h1>Marti</h1>
+        <span>Designer</span>
+        <div>
+            <a href="#">
+                <i style="color:black" class="fa-brands fa-instagram"></i>
+            </a>
+            <a href="#">
+                <i style="color:black" class="fa-solid fa-envelope"></i>
+            </a>
+            <a href="">
+                <i style="color:black" class="fa-brands fa-linkedin"></i>
+            </a>
+        </div>
+        <p>My name is Marti, I am in charge of the design functionality apartment of the page. 
+        I am so excited to take part of this project</p>
+    </div>
 </div>

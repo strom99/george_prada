a3588cc65bfb2fd9968ac19f7095da01a4df8df5
MBP2001 <mbaulenasperez@gmail.com>
2022-03-23 12:45:17 +0100

    hola

--- html/about-us.php
+++ html/about-us.php
@@ -28,28 +28,6 @@
             <p>hello, my name is laura,in charge of the functionality of the page and the database. Excited about
                 keeping this project going</p>
         </div>
-<<<<<<< HEAD
-        <p>hello, my name is laura,in charge of the functionality of the page and the database. Excited about keeping this project going</p>
-    </div>
-    <div>
-        <img style="width: 200px;" src="img/Marti.jpg" alt="">
-        <h1>Marti</h1>
-        <span>Designer</span>
-        <div>
-            <a href="#">
-                <i style="color:black" class="fa-brands fa-instagram"></i>
-            </a>
-            <a href="#">
-                <i style="color:black" class="fa-solid fa-envelope"></i>
-            </a>
-            <a href="">
-                <i style="color:black" class="fa-brands fa-linkedin"></i>
-            </a>
-        </div>
-        <p>My name is Marti, I am in charge of the design functionality apartment of the page. 
-        I am so excited to take part of this project</p>
-    </div>
-=======
         <div class="card">
             <div class="title">
                 <img style="width: 200px;" src="img/alison.jpg" alt="imagen-1">
@@ -72,9 +50,9 @@
         </div>
         <div class="card">
             <div class="title">
-                <img style="width: 200px;" src="img/alison.jpg" alt="imagen-1">
+                <img style="width: 200px;" src="img/Marti.jpg" alt="imagen-1">
                 <h1>Martí</h1>
-                <span>Full Stack Developer</span>
+                <span>Designer</span>
             </div>
             <div>
                 <a href="#">
@@ -87,9 +65,7 @@
                     <i style="color:black" class="fa-brands fa-linkedin"></i>
                 </a>
             </div>
-            <p>hello, my name is laura,in charge of the functionality of the page and the database. Excited about
-                keeping this project going</p>
+            <p>My name is Marti, I am in charge of the functionality design apartment of the page. I am so excited to take part of this project</p>
         </div>
     </main>
->>>>>>> facc4b88bd4748bb5559011f8bf3899f213f6110
 </div>

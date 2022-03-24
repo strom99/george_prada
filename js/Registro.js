fa0a649653272676413f33c0054881958fad20bd
strom99 <alissoncc17@gmail.com>
2022-03-24 11:29:50 +0100

    ddd

---
+++ js/Registro.js
@@ -0,0 +1,16 @@
+$(".formulario").validate({
+    focusCleanup:true,
+    rules:{
+        usuario:{
+            required:true,
+            minlength:2,
+            maxlength:10
+        }
+    },
+    messages:{
+        usuario:{
+            required:"rellena el usuario",
+            minlength:"<span class='alerta'></span>"
+        }
+    }
+})

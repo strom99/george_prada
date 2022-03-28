61350973a53573b1cc7beb10fb1db08cbc3aaa3a
JOM3C4 <jordimejia655@gmail.com>
2022-03-29 00:14:22 +0200

    update

--- index.php
+++ index.php
@@ -16,19 +16,19 @@ $page = $_GET['page'] ?? 'paginaInicio';
     <link rel="stylesheet" href="estilos/<?php echo $page ?>.css">
     <link rel="stylesheet" href="estilos/global.css">
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
     <script src="js/jquery.validate.min.js"></script>
     <script src="js/additional-methods.min.js"></script>
     <script src="https://kit.fontawesome.com/6f213ce53f.js" crossorigin="anonymous"></script>
-    <!-- Libreria Jquery UI -->
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js" integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30=" crossorigin="anonymous"></script>
-    <!-- Libreria Jquery UI -->
     <!--Plugin BxSlider -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
     <!--Plugin BxSlider -->
-</head>
+     <!-- Libreria Jquery UI -->
+     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>    
+    <!-- Libreria Jquery UI -->
+    </head>
 
 <body>
     <?php if (isset($_SESSION['datosUsuario'])) {
@@ -36,13 +36,13 @@ $page = $_GET['page'] ?? 'paginaInicio';
     } ?>
     <?php include "html/$page.php"; ?>
     <!--Añadir mi Scrtipt-->
-
-
+    
+    
     <!-- verificar si existe el archivo js-->
     <?php if (file_exists("js/$page.js")) { ?>
-    <script src="js/<?php echo $page ?>.js"></script>
-    <?php } ?>
-    <?php
+        <script src="js/<?php echo $page ?>.js"></script>
+        <?php } ?>
+        <?php
     if (isset($_SESSION['datosUsuario'])) { ?>
     <script src="js/menu.js"></script>
     <?php } ?>

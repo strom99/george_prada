e7abd0f6f8d7adcf85a920ed8a74da5e924b3b17
strom99 <alissoncc17@gmail.com>
2022-04-29 09:56:38 +0200

    subdia mi perfil jquery

--- js/miPerfil.js
+++ js/miPerfil.js
@@ -1,18 +1,20 @@
-$(".formUpdateName").on('submit' , function(e){
+
+$(".enviar").on('click' , function(e){
     e.preventDefault();
+    $(".formUpdateName").css("width" , "100%");
+    $(".nameInput").css("display","block");
+    $(".cancelar").css("display", "block");
+    $(".nombre").css("display", "none")
+    let nombre = $(".nombre").text();
+    $(".nameInput").val(nombre);
 
-    $(".nameInput").css("display" , "block");
+})
 
-    $.ajax({
-    type:"GET",
-    url:"html/miPerfil.php",
-    dataType:"json",
-    success: function(respJSON){
-        console.log(respJSON);
-        $("#error").html("texto:"+respJSON.data);
-    }
-        
-});
-
+$(".cancelar").on("click", function(e){
+    e.preventDefault();
+    $(".nombre").css("display", "block")
+    $(".formUpdateName").css("width" , "auto");
+    $(".cancelar").css("display", "none");
+    $(".nameInput").css("display","none");
 })
 

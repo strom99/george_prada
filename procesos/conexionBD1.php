57c09ab3c7e36e36c4f1befb390f92321d97002e
strom99 <alissoncc17@gmail.com>
2022-06-07 16:27:36 +0200

    img

--- procesos/conexionBD1.php
+++ procesos/conexionBD1.php
@@ -1,9 +1,9 @@
 <?php
 //objeto 
-$db_host = get_cfg_var("db_host");
-$db_username = get_cfg_var("db_username");
-$db_pass = get_cfg_var("db_pass");
-$db_name = get_cfg_var("db_name");
+$db_host = "localhost";
+$db_username = "root";
+$db_pass = "";
+$db_name = "george_prada";
 
 try {
     // $baseDatos  = new mysqli($db_host, $db_username, $db_pass, $db_name);
@@ -12,5 +12,6 @@ try {
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]);
 } catch (PDOException $e) {
-    throw new Exception('La conexión a la base de datos a fallado: ' . $e->getMessage());
+    throw new Exception('La conexión a la base de datos a fallado');
 }
+
